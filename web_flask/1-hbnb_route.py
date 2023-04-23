8e9b39dd2e8472efbb1131d09906b02c17e4ada5
Olatundeawo <awotimilehin.babatunde@gmail.com>
2023-04-23 14:57:08 +0000

    modify

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -11,6 +11,7 @@ def hello():
     """ return 'Hello HBNB!' """
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """ display 'HBNB' """
@@ -18,4 +19,3 @@ def hbnb():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
